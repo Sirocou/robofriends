1ff344742acdd50cb16efebeea0b4368eb23143d
Sirocco <srizoqulov@bk.ru>
2018-08-19 03:11:01 +0500

    Changed CardList from Component Class to a function

--- src/CardList.js
+++ src/CardList.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import Card from './Card.js';
 import 'tachyons';
 
-class Cards extends Component {
-
-  	render() {
-    	return (
-	    	<div className='tc'>
-	    		{this.props.robots.map(x => <Card id={x.id} name={x.name} email={x.email}/>)}
-	    	</div>
-		);
-	}
+const CardList = ({robots}) => {
+	return (
+		<div className='tc'>
+    		{robots.map(x => <Card id={x.id} name={x.name} email={x.email}/>)}
+    	</div>
+	)
 }
 
-export default Cards;
+export default CardList;
